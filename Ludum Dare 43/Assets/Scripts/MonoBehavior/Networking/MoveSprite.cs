b7cc7470ca36215074d58f208ffd4898319aaeb7
David Birgé-Cotte <david.birge.cotte@gmail.com>
2018-11-30 18:32:41 +0100

    clean disconnect ??

--- Ludum Dare 43/Assets/Scripts/MonoBehavior/Networking/MoveSprite.cs
+++ Ludum Dare 43/Assets/Scripts/MonoBehavior/Networking/MoveSprite.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using BeardedManStudios.Forge.Networking.Generated;
+using BeardedManStudios.Forge.Networking.Unity;
 
 public class MoveSprite : MovingSpriteBehavior 
 {
+
+	protected override void NetworkStart()
+    {
+        base.NetworkStart();
+
+        if (networkObject.IsServer)
+            networkObject.Owner.disconnected += delegate { DestroyServer(); };
+    }
+
 	private void Update () 
 	{
 		if(networkObject == null)
@@ -25,4 +35,10 @@ public class MoveSprite : MovingSpriteBehavior
 
 		networkObject.Position = transform.position;
 	}
+
+	private void DestroyServer()
+    {
+		NetworkManager.Instance.Disconnect(); // devrait tout déconnecter proprement
+        networkObject.Destroy(); // Devrait détruire ce gameobject si cette ligne est appelée ^^
+    }
 }
