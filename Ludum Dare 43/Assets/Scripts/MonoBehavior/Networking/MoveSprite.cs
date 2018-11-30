530c687d99a4e4412c1625a3b1d648e1df881ed4
R-L\R L <r.leblanc@student.isartdigital.com>
2018-11-30 15:34:49 +0200

    Multiplayer test scene

---
+++ Ludum Dare 43/Assets/Scripts/MonoBehavior/Networking/MoveSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MoveSprite : MovingSpriteBehavior 
+{
+	private void Update () 
+	{
+		if (!networkObject.IsServer)
+		{
+			transform.position = networkObject.Position;
+			return;
+		}
+
+		transform.position += new Vector3(
+			Input.GetAxis("Horizontal"),
+			Input.GetAxis("Vertical"),
+			0
+		) * Time.deltaTime * 5f;
+
+		networkObject.Position = transform.position;
+	}
+}
