b4ee27f7813dbeaf89045a5dd1c34d815dc30727
R-L\R L <r.leblanc@student.isartdigital.com>
2018-12-02 16:52:34 +0200

    Goat Sprite changing according to direction

---
+++ LD43/Assets/CodeBase/ClientSide/AnimationManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Sprite))]
+public class AnimationManager : MonoBehaviour 
+{
+	public Sprite Right, Left, Top, Down;
+
+	private Vector2 dir, prevPos;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 pos = new Vector2(transform.position.x, transform.position.y) ;
+		dir = pos - prevPos;
+		DisplayCorrectSprite();
+		prevPos = pos;
+	}
+
+	void DisplayCorrectSprite()
+	{
+		if (dir != Vector2.zero)
+		{
+			dir = dir.normalized;
+			if (dir.x > 0.5f)
+			{
+				sr.sprite = Right;
+			}
+			else if (dir.x < -0.5f)
+			{
+				sr.sprite = Left;
+			}
+			else if(dir.y > -0.5f)
+			{
+				sr.sprite = Top;
+			}
+			else if (dir.y < -0.5f)
+			{
+				sr.sprite = Down;
+			}
+		}
+	}
+}
