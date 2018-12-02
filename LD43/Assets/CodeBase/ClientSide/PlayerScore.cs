da1724d735dfc3648448540ff558f01bc7e78be3
R-L\R L <r.leblanc@student.isartdigital.com>
2018-12-02 13:09:33 +0200

    Auto stash before merge of "master" and "origin/master"
    Adding Score UI

---
+++ LD43/Assets/CodeBase/ClientSide/PlayerScore.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerScore : MonoBehaviour 
+{
+	public TMP_Text PlayerName, Score;
+}
---
+++ LD43/Assets/CodeBase/ClientSide/Score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour 
+{
+	public GameObject ScorePrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateScores();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void UpdateScores()
+	{
+		for (int i = transform.childCount; i > 0; i--)
+		{
+			Destroy(transform.GetChild(i).gameObject);
+		}
+
+		foreach(Player p in FindObjectsOfType<Player>())
+		{
+			GameObject PlayerScore = Instantiate<GameObject>(ScorePrefab, transform);
+			PlayerScore.GetComponent<PlayerScore>().PlayerName.text = p.Name;
+			PlayerScore.GetComponent<PlayerScore>().Score.text = p.Score.ToString();
+		}
+	}
+}
